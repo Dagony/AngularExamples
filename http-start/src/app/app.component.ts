84b3509e8818bdaff0d7d2b5711ceb1ea409f6b1
Dagony <dagony@gmail.com>
2017-12-06 14:11:26 +0100

    Using the Returned Data

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -42,7 +42,7 @@ export class AppComponent {
 
   onGet() {
     this.serverSerivce.getServers().subscribe(
-      (servers: any[]) => console.log(servers),
+      (servers: any[]) => this.servers = servers,
       (error) => console.log(error)
     );
   }
