d35bd898adbe7ed21fecd0a8ecaed0071286bbc4
Dagony <dagony@gmail.com>
2018-01-23 21:20:03 +0100

    The "void" state

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -89,6 +89,46 @@ import {animate, Component, state, style, transition, trigger} from '@angular/co
           ]
         )
       ]
+    ),
+    trigger(
+      'list1',
+      [
+        state(
+          'in',
+          style(
+            {
+              opacity: 1,
+              transform: 'translateX(0)'
+            }
+          )
+        ),
+        transition(
+          'void => *',
+          [
+              style({
+                opacity: 0,
+                transform: 'translateX(-100px)'
+                }
+              ),
+              animate(300)
+            ]
+        ),
+        transition(
+          '* => void',
+          [
+
+            animate(
+              300,
+              style(
+                {
+                  transform: 'translateX(100px)',
+                  opacity: 0
+                }
+              )
+            )
+          ]
+        )
+      ]
     )
   ]
 })
