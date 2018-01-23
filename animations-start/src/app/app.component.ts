160cd5ddc13b599b537978aef164ae9d13733f31
Dagony <dagony@gmail.com>
2018-01-23 20:39:34 +0100

    Advanced Transitions

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,14 +1,77 @@
 import {animate, Component, state, style, transition, trigger} from '@angular/core';
 
 @Component({
-  selector: 'app-root', templateUrl: './app.component.html', animations: [ trigger('divState', [ state('normal', style({
-    'background-color': 'red', transform: 'translateX(0)'
-  })), state('highlighted', style({
-    'background-color': 'blue', transform: 'translateX(100px)'
-  })), transition('normal => highlighted', animate(300)), transition('highlighted => normal', animate(800)) ]) ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style(
+          {
+          'background-color': 'red',
+            transform: 'translateX(0)'
+          }
+        )
+      ),
+      state(
+        'highlighted',
+        style(
+          {
+            'background-color': 'blue',
+            transform: 'translateX(100px)'
+          }
+        )
+      ),
+      transition(
+        'normal <=> highlighted',
+        animate(300)
+      )
+      //,transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state(
+        'normal',
+        style(
+          {
+            'background-color': 'red',
+            transform: 'translateX(0) scale(1)'
+          }
+        )
+      ),
+      state(
+        'highlighted',
+        style(
+          {
+            'background-color': 'blue',
+            transform: 'translateX(100px) scale(1)'
+          }
+        )
+      ),
+      state(
+        'shrunken', style({
+          'background-color': 'green',
+          transform: 'translateX(0) scale(0.5)'
+        })
+      ),
+      transition(
+        'normal => highlighted',
+        animate(300)
+      ),
+      transition(
+        'highlighted => normal',
+        animate(800)
+      ),
+      transition(
+        'shrunken <=> *',
+        animate(500)
+      )
+    ])
+  ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = [ 'Milk', 'Sugar', 'Bread' ];
 
   onAdd(item) {
@@ -21,5 +84,10 @@ export class AppComponent {
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 }
