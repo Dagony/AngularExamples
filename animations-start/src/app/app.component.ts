89b123c7e02ff21d19f7e53f9df1ad70dc0855f9
Dagony <dagony@gmail.com>
2018-01-23 21:03:36 +0100

    Transition Phases

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -4,69 +4,92 @@ import {animate, Component, state, style, transition, trigger} from '@angular/co
   selector: 'app-root',
   templateUrl: './app.component.html',
   animations: [
-    trigger('divState', [
-      state(
-        'normal',
-        style(
-          {
-          'background-color': 'red',
-            transform: 'translateX(0)'
-          }
+    trigger(
+      'divState',
+      [
+        state(
+          'normal',
+          style(
+            {
+              'background-color': 'red',
+              transform: 'translateX(0)'
+            }
+          )
+        ),
+        state(
+          'highlighted',
+          style(
+            {
+              'background-color': 'blue',
+              transform: 'translateX(100px)'
+            }
+          )
+        ),
+        transition(
+          'normal <=> highlighted',
+          animate(300)
         )
-      ),
-      state(
-        'highlighted',
-        style(
-          {
-            'background-color': 'blue',
-            transform: 'translateX(100px)'
-          }
+      ]
+    ),
+    trigger(
+      'wildState',
+      [
+        state(
+          'normal',
+          style(
+            {
+              'background-color': 'red',
+              transform: 'translateX(0) scale(1)'
+            }
+          )
+        ),
+        state(
+          'highlighted',
+          style(
+            {
+              'background-color': 'blue',
+              transform: 'translateX(100px) scale(1)'
+            }
+          )
+        ),
+        state(
+          'shrunken',
+          style(
+            {
+              'background-color': 'green',
+              transform: 'translateX(0) scale(0.5)'
+            }
+          )
+        ),
+        transition(
+          'normal => highlighted',
+          animate(300)
+        ),
+        transition(
+          'highlighted => normal',
+          animate(800)
+        ),
+        transition(
+          'shrunken <=> *',
+          [
+            style(
+              {
+                'background-color': 'orange'
+              }
+            ),
+            animate(
+              1000,
+              style(
+                {
+                  borderRadius: '50px'
+                }
+              )
+            ),
+            animate(500)
+          ]
         )
-      ),
-      transition(
-        'normal <=> highlighted',
-        animate(300)
-      )
-      //,transition('highlighted => normal', animate(800))
-    ]),
-    trigger('wildState', [
-      state(
-        'normal',
-        style(
-          {
-            'background-color': 'red',
-            transform: 'translateX(0) scale(1)'
-          }
-        )
-      ),
-      state(
-        'highlighted',
-        style(
-          {
-            'background-color': 'blue',
-            transform: 'translateX(100px) scale(1)'
-          }
-        )
-      ),
-      state(
-        'shrunken', style({
-          'background-color': 'green',
-          transform: 'translateX(0) scale(0.5)'
-        })
-      ),
-      transition(
-        'normal => highlighted',
-        animate(300)
-      ),
-      transition(
-        'highlighted => normal',
-        animate(800)
-      ),
-      transition(
-        'shrunken <=> *',
-        animate(500)
-      )
-    ])
+      ]
+    )
   ]
 })
 export class AppComponent {
